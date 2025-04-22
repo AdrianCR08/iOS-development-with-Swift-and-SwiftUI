91717f7440dc5c4f68958a4dd4119124df0c6efe
Adrian Ramos <adrianrmz917@gmail.com>
2025-04-21 23:37:57 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Adrian on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
