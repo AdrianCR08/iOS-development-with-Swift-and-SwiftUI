f729a1cf3d51d1569627e494e32a4cee38524c2e
Adrian Ramos <adrianrmz917@gmail.com>
2025-05-04 00:08:12 -0600

    Basic behavior for category 1 finished Added the emojis for Halloween category

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,37 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = [
+        "👻",
+        "🎃",
+        "🧙",
+        "🕷️",
+        "😈",
+        "💀",
+        "🧛",
+        "🧟",
+        "⚰️",
+        "🌙",
+        "🗡️",
+        "👹",
+        "👽",
+        "🩸",
+    ]
+    @State var cardCount: Int = 4
     var body: some View {
+        ScrollView {
+            cards
+                .padding()
+        }
+        Spacer()
+        VStack {
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    var cardCountAdjusters: some View {
         HStack {
-            CardView(isFaceUp:true)
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+
+    var cards: some View {
+        // When the function only has one line of code there is no need to add the return keyword
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(
+            action: {
+                cardCount += offset
+            },
+            label: { Image(systemName: symbol) })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+        
+    }
+
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus")
+    }
+
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus")
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
-    var body: some View {
-        ZStack(content: {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
-            }
+    // Property wrapper to read/write a value managed by SwiftUI, only for small states.
+    let content: String
+    @State var isFaceUp = false
 
-        })
+    var body: some View {
+        ZStack {
+            // Inference type, allow Swift to infer
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1: 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            // To implement the tap functionality we use ViewModifiers
+            // Views are immutable
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
 #Preview {
     ContentView()
-    
+
 }
