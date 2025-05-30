89b935157c78a2ac5c21a329386d6d93ae12c5af
Adrian Ramos <adrianrmz917@gmail.com>
2025-05-29 20:57:51 -0600

    Refactoring to MVVM

---
+++ GameModel.swift
@@ -0,0 +1,28 @@
+//
+//  Category.swift
+//  Memorize
+//
+//  Created by Adrian on 06/05/25.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct Category {
+    let name: String
+    let emojis: [String]
+    let color: Color
+    let symbol: String
+    
+    func duplicateEmojis() -> [String] {
+            return emojis.flatMap { [$0, $0] }
+        }
+    
+    static let all: [Category] = [
+        Category(name: "Animals", emojis: ["🐶", "🐱", "🐭", "🐰", "🦊", "🐻", "🐯", "🐷", "🐸", "🐵", "🦁", "🐮",], color: Color.red, symbol: "pawprint.circle"),
+        Category(name: "Food", emojis: ["🍕", "🍗", "🍔", "🌭", "🌮", "🍟", "🥪", "🍤", "🍙", "🍫", "🥨", "🧁"], color: Color.yellow, symbol: "fork.knife.circle"),
+        Category(name: "Halloween", emojis: ["👻", "🎃", "🧙", "🕷️", "😈", "💀", "🧛", "🧟", "⚰️", "🌙", "🗡️", "👹"], color: Color.orange, symbol: "moon.circle"),
+        Category(name: "Emotions", emojis: ["😀", "🥹", "😅", "😇", "😍", "😜", "😎", "🤩", "🥳", "😭", "😡", "🤢"], color: Color.purple, symbol: "face.smiling"),
+    ]
+}
